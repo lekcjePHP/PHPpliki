ae767eb3754f573ff2b60d88fa6ae343912e55b8
Firstname Lastname <lekcjePHP@gmail.com>
2011-11-12 13:10:15 +0000

    nie dzialajacy foreach

--- index.php
+++ index.php
@@ -7,7 +7,18 @@
 <body>
 
 <?php
+$file  = fopen("text", 'r');
 
+$zawartoscPliku =  fread($file,  filesize('text'));
+
+$tablica = split(' ', $zawartoscPliku);
+echo gettype($tablica);
+
+fore	ach ($tablica as $i ){
+
+}
+
+fclose($file);
 ?>
 </body>
 </html>
